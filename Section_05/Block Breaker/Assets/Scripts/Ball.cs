bd5c3d81f014422e3afeaad200deeeb8d3724f0b
Ian MacLeod <imacleod@users.noreply.github.com>
2018-11-13 18:36:42 -0800

    55 - Update ball position to follow paddle at offset

---
+++ Section_05/Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    // Configuration
+    [SerializeField] Paddle paddle1;
+
+    // State
+    Vector2 ballOffset;
+
+	// Initialization
+	void Start () {
+        ballOffset = transform.position - paddle1.transform.position;
+	}
+	
+	// Called once per frame
+	void Update () {
+        // Ball follows paddle at offset
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + ballOffset;
+	}
+}
