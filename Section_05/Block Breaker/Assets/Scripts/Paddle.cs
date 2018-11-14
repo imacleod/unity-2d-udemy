2a078010e9c50c4bbbd9ca37a2839e779cd55324
Ian MacLeod <imacleod@users.noreply.github.com>
2018-11-13 17:43:05 -0800

    54 - Use Mathf.Clamp to constrain paddle movement

--- Section_05/Block Breaker/Assets/Scripts/Paddle.cs
+++ Section_05/Block Breaker/Assets/Scripts/Paddle.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Paddle : MonoBehaviour {
-
+    // Configuration
+    [SerializeField] float maxX = 15f;
+    [SerializeField] float minX = 1f;
     [SerializeField] float screenWidthInUnits = 16f;
 
 	// Initialization
 	void Start () {
-		
 	}
 	
 	// Called once per frame
 	void Update () {
-        // Set paddle transform position to mouse's position on screen
+        // Set paddle transform position x to mouse's position on screen
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
         float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
-        transform.position = new Vector2(mousePosInUnits, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
+        transform.position = paddlePos;
 	}
 }
