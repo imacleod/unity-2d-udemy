5e4537545fe1fc73dbe6ab91a0f2e70e85434241
Ian MacLeod <imacleod@users.noreply.github.com>
2018-11-13 17:22:09 -0800

    53 - Add paddle component to track mouse position

---
+++ Section_05/Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    [SerializeField] float screenWidthInUnits = 16f;
+
+	// Initialization
+	void Start () {
+		
+	}
+	
+	// Called once per frame
+	void Update () {
+        // Set paddle transform position to mouse's position on screen
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        transform.position = new Vector2(mousePosInUnits, transform.position.y);
+	}
+}
