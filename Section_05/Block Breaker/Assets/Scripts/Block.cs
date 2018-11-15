cdacd26423cf5aa668528e801eef625255cdc944
Ian MacLeod <imacleod@users.noreply.github.com>
2018-11-15 15:24:12 -0800

    64 - Play sound on block destruction

--- Section_05/Block Breaker/Assets/Scripts/Block.cs
+++ Section_05/Block Breaker/Assets/Scripts/Block.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Block : MonoBehaviour {
+    // Configuration
+    [SerializeField] AudioClip breakSound;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Destroy(gameObject);
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
     }
 }
