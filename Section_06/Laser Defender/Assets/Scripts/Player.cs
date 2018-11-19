862f4317bfa89084d9b80544f464508c7d727bce
Ian MacLeod <imacleod@users.noreply.github.com>
2018-11-19 15:29:30 -0800

    87 - Use input for player movement

---
+++ Section_06/Laser Defender/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    // Configuration
+    [SerializeField] float moveSpeed = 10f;
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        var newPosX = transform.position.x + deltaX;
+        var newPosY = transform.position.y + deltaY;
+        transform.position = new Vector2(newPosX, newPosY);
+    }
+
+	// Initialization
+	void Start () {
+		
+	}
+	
+	// Called once per frame
+	void Update () {
+		Move();
+	}
+}
