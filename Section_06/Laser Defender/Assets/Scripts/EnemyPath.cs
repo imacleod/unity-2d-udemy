93ceebbfca3398ed882154b2f7fdba3c7e8648a0
Ian MacLeod <imacleod@users.noreply.github.com>
2018-11-20 15:23:17 -0800

    95 - Add enemy movement along path

--- Section_06/Laser Defender/Assets/Scripts/EnemyPath.cs
+++ Section_06/Laser Defender/Assets/Scripts/EnemyPath.cs
@@ -4,5 +4,38 @@ using UnityEngine;
 
 public class EnemyPath : MonoBehaviour {
     // Configuration
+    int waypointIndex = 0;
+    [SerializeField] float moveSpeed = 5f;
     [SerializeField] List<Transform> waypoints;
+
+    void Start()
+    {
+        transform.position = waypoints[0].transform.position;
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[waypointIndex].transform.position;
+            transform.position = Vector2.MoveTowards(
+                transform.position,
+                targetPosition,
+                moveSpeed * Time.deltaTime);
+
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 }
