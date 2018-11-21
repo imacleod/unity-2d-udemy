b8ac2374e99cde19624c1b405b980e7ff20e3684
Ian MacLeod <imacleod@users.noreply.github.com>
2018-11-21 11:51:27 -0800

    98 - Spawn multiple enemies in wave

---
+++ Section_06/Laser Defender/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+    // Configuration
+    int startWave = 0;
+    [SerializeField] List<WaveConfig> waveConfigs;
+
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
+    {
+        for (int i = 0; i < waveConfig.GetEnemyCount(); i++)
+        {
+            Instantiate(
+                waveConfig.GetEnemyPrefab(),
+                waveConfig.GetWaypoints()[0].transform.position,
+                Quaternion.identity);
+            yield return new WaitForSeconds(waveConfig.GetSpawnWaitTime());
+        }
+    }
+
+	// Initialization
+	void Start () {
+		WaveConfig currentWave = waveConfigs[startWave];
+        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+	}
+}
