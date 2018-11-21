b7498a5cad65fe393e9ae8786a44c59f9e1326d3
Ian MacLeod <imacleod@users.noreply.github.com>
2018-11-21 10:57:35 -0800

    96 - Set up enemy wave

---
+++ Section_06/Laser Defender/Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy Wave Config")]
+public class WaveConfig : ScriptableObject {
+    // Configuration
+    [SerializeField] int enemyCount = 5;
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] float moveSpeed = 2f;
+    [SerializeField] GameObject pathPrefab;
+    [SerializeField] float spawnWaitTime = 0.5f;
+    [SerializeField] float spawnRandomness = 0.3f;
+
+    public int GetEnemyCount() { return enemyCount; }
+    public GameObject GetEnemyPrefab() { return enemyPrefab; }
+    public float GetMoveSpeed() { return moveSpeed; }
+    public GameObject GetPathPrefab() { return pathPrefab; }
+    public float GetSpawnWaitTime() { return spawnWaitTime; }
+    public float GetSpawnRandomness() { return spawnRandomness; }
+}
