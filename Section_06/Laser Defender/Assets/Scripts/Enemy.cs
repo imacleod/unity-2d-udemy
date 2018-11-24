9357ab28c83f3572792ad985eee3033c61c95ded
Ian MacLeod <imacleod@users.noreply.github.com>
2018-11-23 18:31:48 -0800

    104 - Add enemy firing at random intervals

--- Section_06/Laser Defender/Assets/Scripts/Enemy.cs
+++ Section_06/Laser Defender/Assets/Scripts/Enemy.cs
@@ -5,6 +5,22 @@ using UnityEngine;
 public class Enemy : MonoBehaviour {
     // Configuration
     [SerializeField] int health = 100;
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float laserSpeed = 10f;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] float shotCountdown;
+
+    private void CountDownAndShoot()
+    {
+        // Unify shots and game frames
+        shotCountdown -= Time.deltaTime;
+        if (shotCountdown <= 0)
+        {
+            Fire();
+            InitiateShotCountdown();
+        }
+    }
 
     private void DamageTaken(DamageDealer damageDealer)
     {
@@ -15,19 +31,30 @@ public class Enemy : MonoBehaviour {
         }
     }
 
+    private void Fire()
+    {
+        GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity);
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -laserSpeed);  // invert laserSpeed to move down screen
+    }
+
+    private void InitiateShotCountdown()
+    {
+        shotCountdown = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         DamageDealer damageDealer = collision.gameObject.GetComponent<DamageDealer>();
         DamageTaken(damageDealer);
     }
 
-    // Use this for initialization
+    // Initialization
     void Start () {
-		
+        InitiateShotCountdown();
 	}
 	
-	// Update is called once per frame
+	// Called once per frame
 	void Update () {
-		
+        CountDownAndShoot();
 	}
 }
