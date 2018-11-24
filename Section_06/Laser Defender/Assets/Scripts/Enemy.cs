74aacb541032c4d5246beb107f1df1a2608a081b
Ian MacLeod <imacleod@users.noreply.github.com>
2018-11-23 16:54:24 -0800

    103 - Destroy enemy when health reaches zero

--- Section_06/Laser Defender/Assets/Scripts/Enemy.cs
+++ Section_06/Laser Defender/Assets/Scripts/Enemy.cs
@@ -6,14 +6,23 @@ public class Enemy : MonoBehaviour {
     // Configuration
     [SerializeField] int health = 100;
 
+    private void DamageTaken(DamageDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         DamageDealer damageDealer = collision.gameObject.GetComponent<DamageDealer>();
-        health -= damageDealer.GetDamage();
+        DamageTaken(damageDealer);
     }
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
