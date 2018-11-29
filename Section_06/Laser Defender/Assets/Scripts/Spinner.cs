307abf2902fb975da313d1846eaa5915e3679583
Ian MacLeod <imacleod@users.noreply.github.com>
2018-11-28 17:19:58 -0800

    117 - Create projectile sorting layer to render sprites below ships, add spinning to second enemy laser type

---
+++ Section_06/Laser Defender/Assets/Scripts/Spinner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour {
+    float speed;
+
+    // Initialization
+    void Start ()
+    {
+        speed = Random.Range(360f, 720f);
+    }
+
+	// Called once per frame
+	void Update () {
+		transform.Rotate(0, 0, speed * Time.deltaTime);
+	}
+}
