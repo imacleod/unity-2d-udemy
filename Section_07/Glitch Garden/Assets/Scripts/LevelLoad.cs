3563663eb4b7612b853ddf6e5a75fc96af8f49fe
Ian MacLeod <imacleod@users.noreply.github.com>
2020-01-09 17:14:05 -0800

    122 - Begin Glitch Garden game

---
+++ Section_07/Glitch Garden/Assets/Scripts/LevelLoad.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoad : MonoBehaviour
+{
+    [SerializeField] int timeToWait = 4;
+
+    int currentSceneIndex;
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if (currentSceneIndex == 0)
+        {
+            // Wait for splash screen sound to finish
+            StartCoroutine(WaitForTime());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        LoadNextScene();
+    }
+}
